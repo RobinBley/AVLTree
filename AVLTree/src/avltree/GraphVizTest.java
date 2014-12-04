8cbac65c0244800a9312ec7f7f928050a704ec70
Robin <Robin@Robin-PC>
2014-12-04 17:57:58 +0100

    Gui - Mothode zur Oeffnung eines Images Testklasse zur Erstellung eines Graphens via GraphViz

---
+++ AVLTree/src/avltree/GraphVizTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avltree;
+
+import java.io.File;
+
+/**
+ *
+ * @author Robin
+ */
+public class GraphVizTest {
+//    public static void main(String[] args) {
+//               
+//        
+//        //LOCATION IN GraphViz.Java noch aendern
+//        GraphViz gv = new GraphViz();
+//        gv.addln(gv.start_graph());
+//        gv.add("f");
+//        gv.addln("hallo");
+//        
+//        gv.addln("dsf");
+//        gv.add("gasd");
+//        gv.add("ssss");
+//        gv.addln();
+//        gv.addln();
+//        gv.addln("");
+//       // gv.end_graph();
+//        
+//        gv.addln(gv.end_graph());
+//        gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), "jpeg"), "firs.jpeg");
+//        
+//       
+//    }
+    
+}
